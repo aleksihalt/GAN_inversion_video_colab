156b7728cfcef5276d2ad72aceb1790df8721d5b
Aleksi Halttunen <halttunen@aleksis-mbp.home>
2020-11-06 20:26:55 +0000

    i

--- encode_images.py
+++ encode_images.py
@@ -24,7 +24,7 @@ def main():
     parser.add_argument('dlatent_dir', help='Directory for storing dlatent representations')
     parser.add_argument('init_dlatent', default=False, help='path to init dlatent or False')
     parser.add_argument('move_to_folder', default=False, help='path to init dlatent or False')
-    parser.add_argument('--iterations', default=30, help='Number of optimization steps for each batch', type=int)
+    parser.add_argument('--iterations', default=10, help='Number of optimization steps for each batch', type=int)
     parser.add_argument('--network_pkl', default='gdrive:networks/stylegan2-ffhq-config-f.pkl', help='Path to local copy of stylegan2-ffhq-config-f.pkl')
 
     # for now it's unclear if larger batch leads to better performance/quality
